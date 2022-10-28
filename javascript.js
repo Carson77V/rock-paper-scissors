f2a24fdc050a38cac116186d5c6c389c94b1fb00
Carson <carsontwj@gmail.com>
2022-10-28 14:41:57 -0400

    Create set of conditionals if player picks rock
    Add if statements to determine which code will be used when the player
    chooses rock, paper or scissors.
    
    Complete the code under the if statement when player chooses rock. Use switch
    statements and return the proper string from the function.

--- javascript.js
+++ javascript.js
@@ -5,5 +5,27 @@ function getComputerChoice(){
     //Multiply by 3 to get a number between 0 and 2 when rounding down.
     return Math.floor(Math.random() * 3);
 }
+// this function returns a string telling who wins
+function playRound(playerSelection, computerSelection){
+    //if player chooses rock
+    if (playerSelection === 0){
+        //check what computer picked and return appropriate string
+        switch(computerSelection){
+            case 0: return "It's a tie!"
+            break;
+            case 1: return "You Lose! Paper beats Rock"
+            break;
+            default: return "You Win! Rock beats Scissors"
+        }
+    }
+    //if player chooses paper
+    else if (playerSelection === 1){
 
-getComputerChoice();
+    }
+    //if player chooses scissors
+    else {
+
+    }
+}
+
+playRound(0, getComputerChoice());
